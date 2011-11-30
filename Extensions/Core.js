2bfd6d56d6a8597e6f8430f577878df6ba8a11d1
Alexey Chernikov <chernikov.lesha@gmail.com>
2011-11-30 20:02:08 +0200

    Extensional functionality for Prevel

---
+++ Extensions/Core.js
@@ -0,0 +1,169 @@
+/* Prevel Core Extension
+ * (adds additional functionality to the Prevel's Core)
+ * 
+ * Requirements: Core.js
+ * Provides:
+ *  - Support of a couple methods designated on working with arrays from
+ *    the ECMAScript-5 (map, filter, every and so on)
+ *  - A possibility to stringify an object
+ *  - Checking if given object is `window`
+ *  - Attaching scripts and styles
+ *  - Assigning `this` to any of functions
+**/
+
+(function(win, doc, undefined) {
+  var proto = 'prototype',
+      slice = Array[proto].slice,
+      stringify = win.JSON && win.JSON.stringify;
+  
+  pl.extend({
+    // With arrays and objects
+    toArray: function(arrayLikeObject) {
+      return slice.call(arrayLikeObject, 0);
+    },
+    
+    map: function(array, fn) {
+      var output = [];
+      pl.each(array, function(k) {
+        output[k] = fn(this);
+      });
+      return output;
+    },
+    
+    filter: function(array, reservation) {
+      var output = [],
+          key    = -1;
+      pl.each(array.reverse(), function(k, val) {
+        if(reservation(val)) {
+          output[++key] = val;
+        }
+      });
+      return output;
+    },
+     
+    every: function(array, reservation) {
+      var flag = true;
+      pl.each(array, function(k, val) {
+        if(!reservation(val)) {
+          flag = false;
+        }
+      });
+      return flag;
+    },
+    
+    some: function(array, reservation) {
+      var flag = false;
+      pl.each(array, function(k, val) {
+        if(reservation(val)) {
+          flag = true;
+        }
+      });
+      return flag;
+    },
+    
+    unique: function(array) {
+      var a = [];
+      var l = array.length;
+      for(var i = 0; i < l; ++i) {
+        for(var j = i + 1; j < l; ++j) {
+          if(array[i] === array[j]) {
+            j = ++i;
+          }
+        }
+        a.push(array[i]);
+      }
+      return a;
+    },
+    
+    // Is window
+    isWin: function(Obj) {
+      return pl.type(Obj, 'obj') && 'setInterval' in Obj;
+    },
+    
+    // Attach script or css
+    attach: function(params) {
+      var add;
+      params.load = params.load || function() {};
+      
+      if(params.url.substr(-3) === '.js') { 
+        add = pl('<script>', pl.extend({
+          src: params.url, 
+          type: params.type || 'text/javascript'
+        }, params.charset ? {charset: params.charset} : {})).get();
+        
+        // attachEvent doesn't support adding events to objects, so
+        // it's not possible to use `pl.events.attaches.bind`
+        add.onreadystatechange = function(e) {        
+          if(e.readyState === 'complete') {
+            params.load(params.url, +new Date());
+          }
+        };        
+        add.onload = params.load;
+      } else {
+        add = pl('<link>', {
+          href: params.url,
+          rel: 'stylesheet',
+          type: 'text/css'
+        }).get();
+        
+        var sheet, cssRules;
+        if('sheet' in add) {
+          sheet = 'sheet';
+          cssRules = 'cssRules';
+        } else {
+          sheet = 'styleSheet';
+          cssRules = 'rules';
+        }
+        
+        var timeout = setInterval(function() {
+          try {
+            if(add[sheet] && add[sheet][cssRules].length) {
+              clearInterval(timeout);
+              params.load.call(params.url, +new Date());
+           }
+          } catch(e) {}
+        }, 10);
+      }
+      
+      pl('head').append(add);
+      return this;
+    },
+    
+    proxy: function(context, source) {
+      if(!pl.type(context, 'fn')) {
+        return undefined;
+      }
+
+      return function() {
+        return context.apply(
+          source,
+          slice.call(arguments, 2).concat(slice.call(arguments))
+        );
+      };
+    },
+    
+    stringify: stringify ? win.JSON.stringify : function(obj) {
+      var t = pl.type(obj);
+      if(t === 'null') {
+        return String(obj);
+      } else {
+        var n, v, json = [], arr = pl.type(obj, 'arr');
+          
+        for(n in obj) {
+          v = obj[n];
+          t = pl.type(v);
+          
+          if(t === 'str') {
+            v = '"' + v + '"';
+          } else if(t === 'obj') {
+            v = pl.stringify(v);
+          }
+          
+          json.push((arr ? '' : '"' + n + '":') + String(v));
+        }
+        return (arr ? '[' : '{') + String(json) + (arr ? ']' : '}');
+      }
+    }
+  });
+  
+})(this, document);
---
+++ Extensions/Storage.js
@@ -0,0 +1,189 @@
+/* Prevel Storage Extension
+ * (provides functionality for interacting with cookies, localstorage, ...)
+ * 
+ * Requirements: Core.js
+ * Optional requirements: Prevel Core Extension
+ * Provides:
+ *  - Working with Cookies
+ *  - Working with LocalStorage
+ *  - Working with SessionStorage
+ *  - Working with complex Prevel Storage (ls + ss + cookies)
+ *  
+ *  TODO: IndexedDB support
+**/
+
+(function(win, doc, undefined) {
+  
+  var prefixes = {
+        localStorage: 'ls_',
+        sessionStorage: 'ss_'
+      },
+      
+      cookieAsSession = 60 * 60 * 24,
+      cookieAsStorage = cookieAsSession * 31 * 12 * 4,
+      
+      stringify = pl.stringify ? 
+        pl.stringify : 
+        win.JSON && win.JSON.stringify ? 
+          win.JSON.stringify : 
+          function(o) {return o;};
+  
+  pl.extend({
+    getStorage: function(name, type) {
+      var support = !!type;
+      return support ? type.getItem(name) : pl.storage.cookie(prefixes[String(type)] + name);
+    },
+    
+    setStorage: function(name, val, type) {
+      var support = !!type;
+      
+      if(support) {
+        if(pl.type(val, 'obj')) {
+          val = stringify(val);
+        }
+        
+        type.setItem(name, val);
+      } else {
+        pl.storage.cookie.set(prefixes[String(type)] + name, val, {
+          expires: type === 'localStorage' ? cookieAsStorage : cookieAsSession
+        });
+      }
+      
+      return storage[type];
+    },
+    
+    delStorage: function(name, type) {
+      var support = !!type;
+      
+      if(support) {
+        type.removeItem(name);
+      } else {
+        pl.storage.cookie.del(prefixes[String(type)] + name);
+      }
+      
+      return storage[type];
+    }
+  });
+  
+  // Main Router
+  var storage = (function() {
+    return function(name) {
+      return new storage.complexGet(name);
+    };
+  })();
+  
+  pl.extend(storage, {
+    // Storage Cookie Router
+    cookie: (function() {
+      return function(name) {
+        return new storage.cookie.get(name);
+      };
+    })(),
+    
+    // Storage LocalStorage Router
+    ls: (function() {
+      return function(name) {
+        return new storage.ls.get(name);
+      };
+    })(),
+    
+    session: (function() {
+      return function(name) {
+        return new storage.session.get(name);
+      };
+    })(),
+    
+    // Complex storage (based on all the technics)
+    complexGet: function(name) {
+      var token = [
+        storage.cookie(name),
+        storage.ls(name),
+        storage.session(name)
+      ];
+      
+      var ret, _break = false;
+      pl.each(token, function(key, val) {
+        if(_break) return;
+        if(val) {
+          ret = val;
+          _break = true;
+        } 
+      });
+      return ret;
+    },
+    set: function(name, val) {
+      storage.cookie.set(name, val, {expires: cookieAsStorage});
+      storage.session.set(name, val);
+      return storage.ls.set(name, val);
+    },
+    del: function(name) {
+      storage.cookie.del(name);
+      storage.ls.del(name);
+      storage.session.del(name);
+    }
+  });
+  
+  // Storage Cookie Model
+  pl.extend(storage.cookie, {
+    get: function(name) {
+      var matches = doc.cookie.match(new RegExp(
+        '(?:^|; )' + 
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + 
+        '=([^;]*)'
+      ));
+      return matches ? decodeURIComponent(matches[1]) : null;
+    },
+    set: function(name, val, options) {
+      options = options || {};
+      var exp = options.expires;
+      if(exp) {
+        if(exp.toUTCString) {
+          exp = exp.toUTCString();
+        } else if(pl.type(exp, 'int')) {
+          exp = exp * 1000 + (+new Date());
+        }
+        options.expires = exp;
+      }
+
+      var cookie = [name + '=' + encodeURIComponent(val)];
+      for(var o in options) {
+        cookie.push(options[o] === true ? o : o + '=' + options[o]);
+      }
+      doc.cookie = cookie.join('; ');
+
+      return storage.cookie;
+    },
+    del: function(name) {
+      return storage.cookie.set(name, '', {expires: -1});
+    }
+  });
+  
+  // Local Storage Model
+  pl.extend(storage.ls, {
+    get: function(name) {
+      return pl.getStorage(name, localStorage);
+    },
+    set: function(name, val) {
+      return pl.setStorage(name, val, localStorage);
+    },
+    del: function(name) {
+      return pl.delStorage(name, localStorage);
+    }
+  });
+  
+  // Session Storage Model
+  pl.extend(storage.session, {
+    get: function(name) {
+      return pl.getStorage(name, sessionStorage);
+    },
+    set: function(name, val) {
+      return pl.setStorage(name, val, sessionStorage);
+    },
+    del: function(name) {
+      return pl.delStorage(name, sessionStorage);
+    }
+  });
+  
+  pl.extend({storage: storage});
+  
+})(this, document);
