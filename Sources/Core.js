23ed198073432617498ea43d704add7e2e3967b0
Alexey Chernikov <chernikov.lesha@gmail.com>
2012-01-28 01:18:33 +0200

    pl.each() without reverse

--- Sources/Core.js
+++ Sources/Core.js
@@ -150,8 +150,8 @@
     },
     
     each: function(arr, func) {
-      var key = -1;
-      while(++key < arr.length) {
+      var key = arr.length;
+      while(--key > -1) {
         // `this` ought to contains the current value
         func.call(arr[key], key, arr[key]);
       }
