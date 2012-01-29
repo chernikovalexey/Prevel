1a3c5c8106f1413098af40d9eb719f9fa8a68e4c
Alexey Chernikov <chernikov.lesha@gmail.com>
2012-01-29 20:20:05 +0200

    New builder with Node.js

---
+++ Builder/Build.js
@@ -0,0 +1,53 @@
+/* Prevel Builder
+ * 
+ * Provides assembling Prevel Modules and Extensions into two files:
+ * unobfuscated for developers and obfuscated for production.
+**/
+
+(function(DIR, VERSION, undefined) {
+   
+  var fs = require('fs');
+  var cd = require(DIR + 'ComputeDependencies.js');
+  var lc = require(DIR + 'LoadCollection.js');
+  var m  = require(DIR + 'Minify.js');
+  
+  var version = process.argv[2] || VERSION;
+  var modules = (process.argv[3] || 'none').split(',');
+  var extensions = (process.argv[4] || 'none').split(',');
+  
+  function wrap(code, callback) {
+    console.log('Wrapping code with wrap.js..');
+    
+    fs.readFile(DIR + '../Sources/wrap.js', 'utf-8', function(err, f) {
+      callback(f
+        .replace('%version%', version)
+        .replace('  // [Code]', code));
+    });
+  }
+  
+  cd.getQueue(modules, extensions, function(q) {
+    lc.loadQueue(q, function(c) {
+      wrap(c, function(w) {
+        console.log('Bringing amendments in..');
+        
+        fs.writeFile(DIR + '../prevel-full.js', w, function(err) {
+          if(err) {
+            
+          } else {
+            console.log('  prevel-full.js filled successfully.');
+          }
+        });
+        
+        fs.writeFile(DIR + '../prevel-min.js', m.minify(w), function(err) {
+          if(err) {
+            
+          } else {
+            console.log('  prevel-min.js filled successfully.');
+            console.log('Prevel has been built successfully.');
+          }
+        });
+      });
+    });
+  });
+  
+})('./', '1.0.0');
