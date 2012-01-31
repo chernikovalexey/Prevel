4ce42bbca1c906d75c456b012205a0dd6bc7e694
Alexey Chernikov <chernikov.lesha@gmail.com>
2012-01-31 22:43:48 +0200

    Code style fixed, delete is more effecient now

--- Extensions/Observer.js
+++ Extensions/Observer.js
@@ -1,4 +1,6 @@
-/* Module: PubSub.js
+/* Prevel Observer Extension
+ * (provides basic implemenatation of pattern "observer")
+ * 
  * Requirements: Core.js
  * Provides: Publish/Subscribe framework with individual observers
  * 
@@ -6,25 +8,40 @@
  *  - GNU LGPL (http://opensource.org/licenses/lgpl-license.php)
  *  - MIT License (http://opensource.org/licenses/mit-license.php)
 **/
+
 (function() {
+  
   pl.extend({
     Observer: function(fns) {
-      this.fns=fns ? (fns.call ? [fns] : fns) : [];
-      this.subscribe=function(fn) {
+      this.fns = fns ? (fns.call ? [fns] : fns) : [];
+      
+      this.subscribe = function(fn) {
         this.fns.push(fn);
-      }
-      this.unsubscribe=function(fn) {
-        pl.filter(this.fns, function(f){
-          if (f !== fn) {
-            return f;
-          }
+      };
+      
+      this.unsubscribe = function(fn) {
+        var pos = pl.inArray(fn, this.fns);
+        
+        if(~pos) {
+          this.fns.splice(pos, 1);
+        }
+      };
+      
+      this.publish = function(that, args) {
+        if(pl.type(args, 'undef')) {
+          args = that;
+          that = pl;
+        }
+        
+        if(!pl.type(args, 'arr')) {
+          args = [args];
+        }
+        
+        pl.each(this.fns, function() {
+          this.apply(that, args);
         });
-      }
-      this.publish=function(that, args) {
-        pl.each(this.fns, function(){
-          this.apply(that,args);
-        });
-      }
+      };
     }
   });
+  
 })();
