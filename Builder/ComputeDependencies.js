8d530da34b0bc55a6e4e1d6af05a01e433d14d6b
Alexey Chernikov <chernikov.lesha@gmail.com>
2012-02-08 22:07:48 +0200

    Got rid of setTimeouts

--- Builder/ComputeDependencies.js
+++ Builder/ComputeDependencies.js
@@ -1,12 +1,18 @@
 (function(DIR, undefined) {
   
+  // File system API
   var fs = require('fs');
   
+  // Files do not include to the final code
+  // Here it's because wrap wraps the code, but not attend in it
   var forbidden = ['wrap.js'];
+  var k = ['Core.js']; // With the highest importance-coefficient
   var requirements = [];
-  var queue = [[], []];
+  var queue = [[], []]; // What to load
   var frequency = {};
+  var ready = [false, false]; // Fire callback when both equals true
   
+  // Unique given array
   Array.prototype.unique = function() {
     var a = [];
     var l = this.length;
@@ -21,6 +27,18 @@
     return a;
   };
   
+  // Extend the given (`Parent`) object with `Child`;
+  // override existing properties, if `flag` equals to true
+  function extend(Parent, Child, flag) {
+    for(var key in Child) {
+      if((!Parent[key] && !flag) || flag) {
+        Parent[key] = Child[key];
+      }
+    }
+    return Parent;
+  }
+  
+  // Which files requires current file to work properly
   function parseRequirements(path) {
     fs.readFile(path, 'utf-8', function(err, file) {
       var line = file.match(/Requirements:.*(?!\\n)/);
@@ -43,7 +61,7 @@
       if(i === '-') {
         return;
       }
-      
+
       frequency[i] = frequency[i] || 0;
       ++frequency[i];
       
@@ -52,46 +70,57 @@
       }
     });
   }
-    
-  function fillQueue(m, e, callback) {
-    if(m[0] === '*') {
-      fs.readdir(DIR + 'Sources', function(err, files) {
-        files.forEach(function(f) {
-          if(~forbidden.indexOf(f)) {
-            return;
-          }
-          
-          queue[0].push(f);
-          parseRequirements(DIR + 'Sources/' + f);
-        });
-      });
-    } else if(m[0] !== 'none') {
-      m.forEach(function(i) {
-        i = i[0].toUpperCase() + i.substr(1) + '.js';
-        queue[0].push(i);
-        parseRequirements(DIR + 'Sources/' + i);
-      });
-    }
-    
-    if(e[0] === '*') {
-      fs.readdir(DIR + 'Extensions', function(err, files) {
-        files.forEach(function(f) {
-          queue[1].push(f);
-          parseRequirements(DIR + 'Extensions/' + f);
-        });
-      });
-    } else if(e[0] !== 'none') {
-      e.forEach(function(i) {
-        i = i[0].toUpperCase() + i.substr(1) + '.js';
-        queue[1].push(i);
-        parseRequirements(DIR + 'Extensions/' + i);
-      });
-    }
+  
+  // Handed down in the separate function, because variable name can be changed
+  var ready_pos = 0;
+  function setReady() {
+    ready[ready_pos++] = true;
   }
   
+  function fillQueue(m, e, callback) {
+    var common = function(fe, num, forbid, folder) {
+      if(fe[0] === '*') {
+        fs.readdir(DIR + folder, function(err, files) {
+          files.forEach(function(f) {
+            if(~forbidden.indexOf(f) && forbid) {
+              return;
+            }
+            
+            queue[num].push(f);
+            parseRequirements(DIR + folder + '/' + f);
+          });
+          setReady();
+        });
+      } else if(fe[0] !== 'none') {
+        fe.forEach(function(i) {
+          i = i[0].toUpperCase() + i.substr(1) + '.js';
+          queue[num].push(i);
+          parseRequirements(DIR + folder + '/' + i);
+        });
+        setReady();
+      } else if(fe[0] === 'none') {
+        setReady();
+      }
+    };
+    
+    common(m, 0, true, 'Sources');
+    common(e, 1, false, 'Extensions');
+  }
+    
   function orderQueue(callback) {
     var _q = [];
-        
+    
+    var temp = {};
+    k.forEach(function(i) {
+      for(var key in frequency) {
+        if(i === key) {
+          temp[key] = frequency[key];
+        }
+      }
+    });
+    
+    frequency = extend(temp, frequency);
+    
     for(var key in frequency) {
       queue[0].forEach(function(i) {
         if(i === key) {
@@ -105,7 +134,7 @@
         _q.push(i);
       }
     });
-    
+        
     queue[0] = _q;
     callback();
   }
@@ -115,13 +144,16 @@
     
     fillQueue(m, e);
     
-    setTimeout(function() {
-      queue[0] = queue[0].concat(requirements);
-      queue[0] = queue[0].unique();
-      orderQueue(function() {
-        callback(queue);
-      });
-    }, 100);
+    var int = setInterval(function() {
+      if(ready[0] && ready[1]) {
+        queue[0] = queue[0].concat(requirements);
+        queue[0] = queue[0].unique();
+        orderQueue(function() {
+          callback(queue);
+        });
+        clearInterval(int);
+      }
+    }, 1);
   }
   
   // Export
