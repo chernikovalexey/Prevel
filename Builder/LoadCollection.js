a9c7c32ad19ca95d31938838b31550bccbf83643
Alexey Chernikov <chernikov.lesha@gmail.com>
2012-02-08 22:09:03 +0200

    Prevel Core loads always before other code now (it was a bug)

--- Builder/LoadCollection.js
+++ Builder/LoadCollection.js
@@ -2,27 +2,41 @@
   
   var fs = require('fs');
   
-  function loadQueue(queue, callback) {
+  function loadQueue(q, callback) {
     console.log('Loading files..');
     
+    var padding = ['Core.js']; // Files which should be inserted before
+    var before = ''; // Will be inserted before other code
     var code = '';
+    var container = 0;
+    var complex = q[0].length + q[1].length;
+    
+    q[0].forEach(function(i) {
+      if(~padding.indexOf(i)) {
+        before = fs.readFileSync(MODULES + i, 'utf-8');
+      } else {
+        code += fs.readFileSync(MODULES + i, 'utf-8');
+      }
+      
+      ++container;
+    });
+
+    q[1].forEach(function(i) {
+      code += fs.readFileSync(EXT + i, 'utf-8');
+      ++container;
+    });
+    
+    code = before + code;
+    delete before;
+    
+    var int = setInterval(function() {
+      if(container === complex) {
+        console.log('  OK.');
         
-    queue[0].forEach(function(i) {
-      fs.readFile(MODULES + i, function(err, f) {
-        code += f;
-      });
-    });
-    
-    queue[1].forEach(function(i) {
-      fs.readFile(EXT + i, function(err, f) {
-        code += f;
-      });
-    });
-    
-    setTimeout(function() {
-      console.log('  OK.');
-      callback(code);
-    }, 25);
+        callback(code);
+        clearInterval(int);
+      }
+    }, 1);
   }
   
   module.exports.loadQueue = loadQueue;
